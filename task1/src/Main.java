546c872342457acc03282de30389f22d83d6195d
pestoff7 <ilya.pestov.03@gmail.com>
2022-09-07 10:36:42 +0400

    second commit

--- task1/src/Main.java
+++ task1/src/Main.java
@@ -7,12 +7,14 @@ public class Main {
         int n = input.nextInt();
         int m = input.nextInt();
         int i = 1;
+        String size = "";
         while (true){
-            System.out.print(i);
+            size = size + i;
             i = 1 + (i + m - 2) % n;
             if (i == 1) {
                 break;
             }
         }
+        System.out.println(size);
     }
 }
